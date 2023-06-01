c38ec715c733ef5171ec1a4d3d1d634f37117c7d
Arthur Caumartin <artcaumart@gmail.com>
2023-06-01 10:54:03 +0200

    cinematic inspector revue

--- ChronoMountain/Assets/[Project]/Scripts/Scriptable Object/ScriptableCinematic.cs
+++ ChronoMountain/Assets/[Project]/Scripts/Scriptable Object/ScriptableCinematic.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using AYellowpaper.SerializedCollections;
 
 [CreateAssetMenu(fileName = "ScriptableCinematic", menuName = "ChronoMoutain/ScriptableCinematic")]
 public class ScriptableCinematic : ScriptableObject
 {
-    public List<Sprite> backGroundSprite;
-    public List<string> textList;
+    [System.Serializable]
+    public struct Data
+    {
+        public Sprite backGround;
+        public string text;
+        public float printDuration;
+    }
+    public List<Data> dataList;
+
+    // public List<Sprite> backGroundSprite;
+    // public List<string> textList;
 }
